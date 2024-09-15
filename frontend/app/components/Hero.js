2e3fb61070c59401ea04045ef049c75f42562d62
Antony Dominguez <jaconsuegra@uninorte.edu.co>
2024-09-14 23:55:11 -0500

    Creación del Hero

---
+++ frontend/app/components/Hero.js
@@ -0,0 +1,17 @@
+export default function Hero() {
+    return (
+        <div className="max-w-screen">
+            <div className="flex flex-col gap-4 mb-4 text-center px-2">
+                <h1 className="text-4xl mt-5 font-bold tracking-tight text-gray-800 sm:text-5xl sm:mt-5 dark:text-white">
+                    Laboratorio 1: experimentos con problemas de optimización
+                </h1>
+                <p
+                    className="mt-6 text-xl text-gray-800"
+                >
+                    Validar de manera práctica los conceptos básicos de optimización
+                </p>
+            </div>
+        
+        </div>
+    );
+}
