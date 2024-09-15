8f7f78c34e6cfc46dad994980f506442c2030e8c
Antony Dominguez <jaconsuegra@uninorte.edu.co>
2024-09-15 13:45:39 -0500

    Creación de un container para las secciones del lab

---
+++ frontend/app/components/ContainerSection.js
@@ -0,0 +1,8 @@
+export default function ContainerSection({ children }) {
+    return (
+        <div className="scroll-m-20 w-full mx-auto container max-w-screen">
+            {children}
+        </div>
+    )
+
+}
