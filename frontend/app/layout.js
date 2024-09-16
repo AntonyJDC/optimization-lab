2ced3d368f47e9e209a1c3a3b0c7d8ae4a50e6eb
Antony Dominguez <106288728+AntonyJDC@users.noreply.github.com>
2024-09-16 12:49:56 -0500

    Update layout.js

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -22,10 +22,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <head>
-        <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
-        <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
-      </head>
       <body
         className="relative text-black dark:text-white"
       >
